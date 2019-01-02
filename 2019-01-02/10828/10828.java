09c5453eb2730a039d12f58942f9850ded46aea9
dl57934 <dl57934@pukyong.ac.kr>
2019-01-03 04:38:14 +0900

    solve 10828 by Java

---
+++ 2019-01-02/10828/10828.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+	static Stack<Integer> stack = new Stack<Integer>(); 
+
+	final static String PUSH = "push";
+	final static String POP = "pop";
+	final static String EMPTY = "empty";
+	final static String TOP = "top";
+	final static String SIZE = "size";
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(bfr.readLine());
+
+		for(int i = 0; i < N; i++){
+			String[] command = bfr.readLine().split(" ");
+
+			if(command[0].equals(PUSH))
+				push(Integer.parseInt(command[1]));
+
+			else if(command[0].equals(POP) || command[0].equals(TOP)){
+				if(isEmpty())
+					printResult(-1);
+				else{
+					printResult(stack.peek());
+					if(command[0].equals(POP))
+					stack.pop();
+				}
+			}
+
+			else if(command[0].equals(EMPTY)){
+				if(isEmpty())
+					printResult(1);
+				else
+					printResult(0);
+			}
+			
+			else if(command[0].equals(SIZE)){
+				printResult(stack.size());
+			}
+			
+		}
+	}
+
+	private static Boolean isEmpty(){
+		return stack.empty();
+	}
+
+
+	private static void printResult(int num){
+		System.out.println(num);
+	}
+
+	private static void push(int num){
+		stack.push(num);
+	}
+}
