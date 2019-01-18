bdf99b15f315927dd8f19f9d0e9e943df713e3c5
dl57934 <dl57934@pukyong.ac.kr>
2019-01-18 19:12:50 +0900

    solve 9375 by c++

--- 2019-01-18/9375/9375.cpp
+++ 2019-01-18/9375/9375.cpp
@@ -1,11 +1,33 @@
 #include <cstdio>
 #include <string>
 #include <map>
-
+#include <iostream>
 using namespace std;
 
+map <string, int>info;
+string order[102];
+string type[102];
 int main(){
 	int T, n;
-	scanf("%d %d", &T, &n);
-	
+	scanf("%d", &T);
+	int result = 1;
+	for(int i =0; i < T; i++){
+		scanf("%d", &n);
+		for(int i =0; i < n; i++){
+			cin >> order[i] >> type[i];
+			if(info.find(type[i]) == info.end()){
+				info[type[i]] = 1;
+			}
+			else{
+				info[type[i]] += 1;
+
+				}
+		}
+		for (map<string,int>::iterator j= info.begin(); j != info.end(); j++){
+			 result *= j->second+1;
+			 }
+		cout << result-1<<endl;	
+		info.clear();
+		result = 1;
+	}
 }
