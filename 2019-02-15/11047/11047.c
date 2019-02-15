e161235d404fd529d1668516e0ea7d7a3b81d1ed
dl57934 <dl57934@pukyong.ac.kr>
2019-02-15 20:26:14 +0900

    11047 <= 한번 더 생각해보기

---
+++ 2019-02-15/11047/11047.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int coins[11]={0, };
+
+int main(){
+	int N, K;
+	int result = 0;
+	scanf("%d %d", &N, &K);
+	for(int i = 0; i < N; i++)
+		scanf("%d", &coins[i]);
+	
+
+	for(int i = N-1; i >=0; i--){
+		if(K >= coins[i]){
+			printf("%d: %d\n", coins[i],K/coins[i]);
+			result += K/coins[i];
+			K=K%coins[i];
+		}else if(K==0){
+			break;
+		}
+	}
+	printf("%d", result);
+}
