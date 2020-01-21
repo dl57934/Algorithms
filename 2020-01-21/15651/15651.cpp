7543d87b4feaff62a879957d919640ddd37470b5
이상훈 <dl57934@naver.com>
2020-01-21 14:56:46 +0900

    review 수열 출력

---
+++ 2020-01-21/15651/15651.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int N, M;
+int arr[10];
+
+void getPermutation(int limit);
+
+int main(){
+	scanf("%d %d", &N, &M);
+	getPermutation(0);
+}
+
+void getPermutation(int limit){
+	if(limit == M){
+		for(int i = 0; i < M; i++)
+			printf("%d ", arr[i]);
+		printf("\n");
+		return ;
+	}
+
+	for(int i = 0; i < N; i++){	
+		if(arr[limit-1] <= i+1 || limit==0){
+			arr[limit] = i+1;
+			getPermutation(limit+1);
+		}
+	}
+}
