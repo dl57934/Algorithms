29f3e18186f158dba3b3088b3e2c109d34a5977b
이상훈 <dl57934@naver.com>
2020-03-27 13:47:40 +0900

    review Algorithm

--- 2020-03-25/10942/10942.cpp
+++ 2020-03-25/10942/10942.cpp
@@ -1,45 +1,42 @@
 #include <cstdio>
 #include <string.h>
-int numbers[2002];
-int dp[2002][2002];
+
 int N, S, E, M;
+int dp[2002][2002];
+int data[2002];
 
 void input();
-int getPalin(int start, int end);
+int isPalin(int start, int end);
 
 int main(){
+	memset(dp, -1, sizeof(dp));
 	input();
-	// for(int i = 1; i <= N; i++){
-	// 	for(int x = 1; x <= N; x++){
-	// 		printf("%d ", dp[i][x]);
-	// 	}
-	// 	printf("\n");
-	// }
 }
 
 void input(){
-	memset(dp, -1, sizeof(dp));
 	scanf("%d", &N);
 	for(int i = 1; i <= N; i++)
-		scanf("%d", &numbers[i]);
+		scanf("%d", &data[i]);
 	scanf("%d", &M);
-	for(int i = 1; i <= M; i++){
+	for(int i = 1;i <= M; i++){
 		scanf("%d %d", &S, &E);
-		printf("%d\n", getPalin(S, E));
+		printf("%d\n", isPalin(S, E));
 	}
 }
 
-int getPalin(int start, int end){
+
+int isPalin(int start, int end){
 	if(dp[start][end] != -1){
 		return dp[start][end];
 	}
-	if (start > end){
+
+	if(start > end)
 		return 1;
-	}else if(numbers[start] != numbers[end] ){
+	else if(data[start] != data[end]){
 		dp[start][end] = 0;
 		return 0;
 	}else{
-		dp[start][end] = getPalin(start+1, end-1);
+		dp[start][end] = isPalin(start+1, end-1);
 		return dp[start][end];
 	}
 }
